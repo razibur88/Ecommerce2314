2b0ee8bdfe4b081c6efa046dfde727f08917a318
mern2301 <mern2301@gmail.com>
2024-08-13 12:21:05 +0600

    Demo

--- src/components/pagination/PaginationArea.jsx
+++ src/components/pagination/PaginationArea.jsx
@@ -1,40 +1,58 @@
-import React from 'react'
+import React from "react";
 
-const PaginationArea = ({pageNumber,paginate,currentPage,next,prev}) => {
+const PaginationArea = ({
+  pageNumber,
+  paginate,
+  currentPage,
+  next,
+  prev,
+  mobileview,
+}) => {
   return (
     <>
-  <nav className='py-3 mt-[50px] text-center lg:text-end' aria-label="Page navigation example">
-    <ul className="inline-flex -space-x-px text-sm">
-      {pageNumber.length > 0 &&
-        <li onClick={prev}>
-        <a className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-        >
-          Previous
-        </a>
-      </li>
-      }
-      
-      {pageNumber.map((item, i)=>(
-      <li onClick={()=>paginate(item)} className={currentPage == i + 1 ? "flex items-center justify-center px-3 h-8 leading-tight text-white border border-gray-300 bg-[#262626] cursor-pointer" : "flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 cursor-pointer"}>
-        {item + 1}
-        
-      </li>
-      ))}
-      
-      {pageNumber.length > 0 && 
-        <li>
-        <a onClick={next} className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-        >
-          Next
-        </a>
-      </li>
-      }
-    </ul>
-  </nav>
- 
-</>
+      <nav
+        className="py-3 mt-[50px] text-center lg:text-end"
+        aria-label="Page navigation example"
+      >
+        <ul className="inline-flex -space-x-px text-sm">
+          {pageNumber.length > 0 && (
+            <li onClick={prev}>
+              <a className="cursor-pointer flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                Previous
+              </a>
+            </li>
+          )}
+          {mobileview && (
+            <>
+              {pageNumber.map((item, i) => (
+                <li
+                  onClick={() => paginate(item)}
+                  className={
+                    currentPage == i + 1
+                      ? "flex items-center justify-center px-3 h-8 leading-tight text-white border border-gray-300 bg-[#262626] cursor-pointer"
+                      : "flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 cursor-pointer"
+                  }
+                >
+                  {item + 1}
+                </li>
+              ))}
+            </>
+          )}
 
-  )
-}
+          {pageNumber.length > 0 && (
+            <li>
+              <a
+                onClick={next}
+                className="cursor-pointer lg:ml-0 ml-3  flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              >
+                Next
+              </a>
+            </li>
+          )}
+        </ul>
+      </nav>
+    </>
+  );
+};
 
-export default PaginationArea
+export default PaginationArea;
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,140 +1,226 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from "react";
 import { FaCaretDown, FaCaretUp } from "react-icons/fa";
 import { GrSort } from "react-icons/gr";
 import { PiSquaresFourFill } from "react-icons/pi";
-import Container from '../components/Container';
-import { apiData } from '../components/ContextApi';
-import PaginationArea from '../components/pagination/PaginationArea';
-import Post from '../components/pagination/Post';
-
-
+import Container from "../components/Container";
+import { apiData } from "../components/ContextApi";
+import PaginationArea from "../components/pagination/PaginationArea";
+import Post from "../components/pagination/Post";
 
 const Product = () => {
-  let data = useContext(apiData)
-  let [currentPage, setCurrentpage] = useState(1)
-  let [perPage, setPerPage] = useState(9)
-  let [catshow, setCatShow] = useState(false)
-  let [category, setCategory] = useState([])
-  let [categorySearchFilter, setCategorySearchFilter] = useState([])
-  let [multiList, setMultiList] = useState('')
-  let [lowprice, setLowprice] = useState("")
-  let [highprice, setHighprice] = useState("")
-  let [filterPrice, setFilterPrice] = useState([])
+  let data = useContext(apiData);
+  let [currentPage, setCurrentpage] = useState(1);
+  let [perPage, setPerPage] = useState(9);
+  let [catshow, setCatShow] = useState(false);
+  let [category, setCategory] = useState([]);
+  let [categorySearchFilter, setCategorySearchFilter] = useState([]);
+  let [multiList, setMultiList] = useState("");
+  let [lowprice, setLowprice] = useState("");
+  let [highprice, setHighprice] = useState("");
+  let [filterPrice, setFilterPrice] = useState([]);
+  let [mobileview, setMobileview] = useState(true);
 
+  let lastPage = currentPage * perPage;
+  let firstPage = lastPage - perPage;
 
-  let lastPage = currentPage * perPage
-  let firstPage = lastPage - perPage
+  let allData = data.slice(firstPage, lastPage);
 
-  let allData = data.slice(firstPage, lastPage)
+  let pageNumber = [];
 
-  let pageNumber = []
-
-  for (let i = 0; i < Math.ceil(categorySearchFilter.length > 0 ? categorySearchFilter : data.length / perPage); i++) {
-    pageNumber.push(i)
+  for (
+    let i = 0;
+    i <
+    Math.ceil(
+      categorySearchFilter.length > 0
+        ? categorySearchFilter
+        : data.length / perPage
+    );
+    i++
+  ) {
+    pageNumber.push(i);
   }
 
   let paginate = (pageNumber) => {
     setCurrentpage(pageNumber + 1);
-  }
+  };
 
   let next = () => {
     if (currentPage < pageNumber.length) {
-      setCurrentpage((state) => state + 1)
+      setCurrentpage((state) => state + 1);
     }
-  }
+  };
 
   let prev = () => {
     if (currentPage > 1) {
-      setCurrentpage((state) => state - 1)
+      setCurrentpage((state) => state - 1);
     }
-  }
+  };
   useEffect(() => {
-    setCategory([...new Set(data.map((item) => item.category))])
-  }, [data])
+    setCategory([...new Set(data.map((item) => item.category))]);
+  }, [data]);
 
   let handleSubcate = (citem) => {
-    let categoryFilter = data.filter((item) => item.category == citem)
-    setCategorySearchFilter(categoryFilter)
-  }
+    let categoryFilter = data.filter((item) => item.category == citem);
+    setCategorySearchFilter(categoryFilter);
+  };
 
   let handleList = () => {
     setMultiList("activeList");
-  }
-  let handlePrice = (value) =>{
-    setLowprice(value.low)
-    setHighprice(value.high)
-    let priceFilter = data.filter((item)=>item.price > value.low && item.price < value.high)
+  };
+  let handlePrice = (value) => {
+    setLowprice(value.low);
+    setHighprice(value.high);
+    let priceFilter = data.filter(
+      (item) => item.price > value.low && item.price < value.high
+    );
     setFilterPrice(priceFilter);
-  }
+  };
 
+  let handleShowData = (e) => {
+    setPerPage(e.target.value);
+  };
+
+  useEffect(() => {
+    function navbarMenu() {
+      if (window.innerWidth < 1024) {
+        setMobileview(false);
+      } else {
+        setMobileview(true);
+      }
+    }
+    navbarMenu();
+    window.addEventListener("resize", navbarMenu);
+  }, []);
 
   return (
     <section>
       <Container>
-        <div className='lg:flex'>
+        <div className="lg:flex">
           <div className="lg:w-[20%] w-full ">
             <div className="mt-16 pl-5">
-              <h3 onClick={() => setCatShow(!catshow)} className='font-sans lg:text-[20px] text-[12px] font-bold text-[#262626] flex justify-between items-center cursor-pointer'>Shop by Category <p>{catshow == true ? <FaCaretUp /> : <FaCaretDown />}</p></h3>
-              {catshow &&
+              <h3
+                onClick={() => setCatShow(!catshow)}
+                className="font-sans lg:text-[20px] text-[12px] font-bold text-[#262626] flex justify-between items-center cursor-pointer"
+              >
+                Shop by Category{" "}
+                <p>{catshow == true ? <FaCaretUp /> : <FaCaretDown />}</p>
+              </h3>
+              {catshow && (
                 <ul>
                   {category.map((item) => (
-
-                    <li onClick={() => handleSubcate(item)} className='font-sans lg:text-[16px] text-[12px] font-normal text-[#767676] lg:py-5 py-2 border-b-2 border-[#F0F0F0] capitalize'>{item}</li>
+                    <li
+                      onClick={() => handleSubcate(item)}
+                      className="font-sans lg:text-[16px] text-[12px] font-normal text-[#767676] lg:py-5 py-2 border-b-2 border-[#F0F0F0] capitalize"
+                    >
+                      {item}
+                    </li>
                   ))}
                 </ul>
-              }
+              )}
             </div>
             <div className="mt-16 pl-5">
-              <h3 className='font-sans lg:text-[20px] text-[12px] font-bold text-[#262626] flex justify-between items-center cursor-pointer'>Shop by Price</h3>
-              
-                <ul>
-                    <li onClick={()=>handlePrice({low:0, high:10})} className='font-sans lg:text-[16px] text-[12px] font-normal text-[#767676] lg:py-5 py-2 border-b-2 border-[#F0F0F0] capitalize'>Price $0- $10</li>
-                    <li onClick={()=>handlePrice({low:10, high:20})} className='font-sans lg:text-[16px] text-[12px] font-normal text-[#767676] lg:py-5 py-2 border-b-2 border-[#F0F0F0] capitalize'>Price $10- $20</li>
-             
-                </ul>
-              
+              <h3 className="font-sans lg:text-[20px] text-[12px] font-bold text-[#262626] flex justify-between items-center cursor-pointer">
+                Shop by Price
+              </h3>
+
+              <ul>
+                <li
+                  onClick={() => handlePrice({ low: 0, high: 10 })}
+                  className="font-sans lg:text-[16px] text-[12px] font-normal text-[#767676] lg:py-5 py-2 border-b-2 border-[#F0F0F0] capitalize"
+                >
+                  Price $0- $10
+                </li>
+                <li
+                  onClick={() => handlePrice({ low: 10, high: 20 })}
+                  className="font-sans lg:text-[16px] text-[12px] font-normal text-[#767676] lg:py-5 py-2 border-b-2 border-[#F0F0F0] capitalize"
+                >
+                  Price $10- $20
+                </li>
+              </ul>
             </div>
           </div>
-          
-          
+
           <div className="lg:w-[80%] w-full">
             <div className="lg:flex justify-between pt-[10px] mt-[50px] pl-10">
               <div className="flex gap-x-5 lg:justify-normal justify-center lg:w-[30%] w-100%">
-                <div onClick={() => setMultiList("")} className={`lg:h-[46px] h-[26px] lg:w-[46px] w-[26px] flex justify-center items-center ${multiList == "activeList" ? "bg-white text-white" : "bg-black text-white"} border-[1px] border-[#737373] text-[20px]`}><PiSquaresFourFill /></div>
-                <div onClick={handleList} className="lg:h-[46px] h-[26px] lg:w-[46px] w-[26px] flex justify-center items-center text-[#737373] hover:text-white duration-300 ease-in-out hover:bg-black border-[1px] border-[#737373] text-[20px]"><GrSort /></div>
+                <div
+                  onClick={() => setMultiList("")}
+                  className={`lg:h-[46px] h-[26px] lg:w-[46px] w-[26px] flex justify-center items-center ${
+                    multiList == "activeList"
+                      ? "bg-white text-white"
+                      : "bg-black text-white"
+                  } border-[1px] border-[#737373] text-[20px]`}
+                >
+                  <PiSquaresFourFill />
+                </div>
+                <div
+                  onClick={handleList}
+                  className="lg:h-[46px] h-[26px] lg:w-[46px] w-[26px] flex justify-center items-center text-[#737373] hover:text-white duration-300 ease-in-out hover:bg-black border-[1px] border-[#737373] text-[20px]"
+                >
+                  <GrSort />
+                </div>
               </div>
               <div className="flex lg:justify-end justify-between lg:gap-x-10 lg:w-[70%] w-[100%] pt-[20px] lg:pt-0">
                 <div className="">
-                  <label htmlFor="" className='text-[16px] font-sans text-[#767676] font-normal'>Sort by : </label>
-                  <select name="" id="" className='border-[1px] border-[#767676] rounded-sm text-start lg:px-[60px] px-[10px] py-[5px] text-[16px] font-sans text-[#767676] font-normal'>
+                  <label
+                    htmlFor=""
+                    className="text-[16px] font-sans text-[#767676] font-normal"
+                  >
+                    Sort by :{" "}
+                  </label>
+                  <select
+                    name=""
+                    id=""
+                    className="border-[1px] border-[#767676] rounded-sm text-start lg:px-[60px] px-[10px] py-[5px] text-[16px] font-sans text-[#767676] font-normal"
+                  >
                     <option value="">Featured</option>
                     <option value="">Price</option>
                     <option value="">Category</option>
                   </select>
                 </div>
                 <div className="">
-                  <label htmlFor="" className='text-[16px] font-sans text-[#767676] font-normal'>Show : </label>
-                  <select name="" id="" className='border-[1px] border-[#767676] rounded-sm text-start lg:px-[60px] px-[25px] py-[5px] text-[16px] font-sans text-[#767676] font-normal'>
-                    <option value="">36</option>
-                    <option value="">30</option>
-                    <option value="">24</option>
+                  <label
+                    htmlFor=""
+                    className="text-[16px] font-sans text-[#767676] font-normal"
+                  >
+                    Show :{" "}
+                  </label>
+                  <select
+                    onChange={handleShowData}
+                    name=""
+                    id=""
+                    className="border-[1px] border-[#767676] rounded-sm text-start lg:px-[60px] px-[25px] py-[5px] text-[16px] font-sans text-[#767676] font-normal"
+                  >
+                    <option value="9">9</option>
+                    <option value="12">12</option>
+                    <option value="18">18</option>
                   </select>
                 </div>
               </div>
             </div>
 
-            <Post filterPrice={filterPrice} allData={allData} categorySearchFilter={categorySearchFilter} multiList={multiList} />
-
+            <Post
+              filterPrice={filterPrice}
+              allData={allData}
+              categorySearchFilter={categorySearchFilter}
+              multiList={multiList}
+            />
 
             <div className="text-end">
-              <PaginationArea pageNumber={pageNumber} paginate={paginate} currentPage={currentPage} next={next} prev={prev} />
+              <PaginationArea
+                pageNumber={pageNumber}
+                paginate={paginate}
+                currentPage={currentPage}
+                next={next}
+                prev={prev}
+                mobileview={mobileview}
+              />
             </div>
-
           </div>
         </div>
       </Container>
     </section>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
